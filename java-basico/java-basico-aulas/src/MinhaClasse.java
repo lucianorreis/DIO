224902b8ef75c07e19c6fcf096fa5eadc55bed0e
Luciano.Reis <lrr.arms233@gmail.com>
2024-08-10 19:16:48 -0300

    Update MinhaClasse Code

--- java-basico/java-basico-aulas/src/MinhaClasse.java
+++ java-basico/java-basico-aulas/src/MinhaClasse.java
@@ -1,5 +1,15 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        
+
+        String primeiroNome = "Luciano";
+        String segundoNome = "Reis";
+
+        String NomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(NomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
