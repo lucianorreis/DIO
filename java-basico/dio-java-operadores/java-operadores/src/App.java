a7d900923a528a57f5ef9563e58f9ad8fa3bca56
Luciano.Reis <lrr.arms233@gmail.com>
2024-08-19 20:49:07 -0300

    Add Notes to App.java

--- java-basico/dio-java-operadores/java-operadores/src/App.java
+++ java-basico/dio-java-operadores/java-operadores/src/App.java
@@ -3,3 +3,24 @@ public class App {
         System.out.println("Hello, World!");
     }
 }
+
+/*
+ * Operadores
+ * 
+ * -----------------------------------------------------------
+ * Atribuição:
+ ? representado pelo símbolo de " = " (igual),
+ ? Strings são atribuídas com aspas
+ * -----------------------------------------------------------
+ * Aritméticos:
+ * Utilizados para realizar operações aritméticas.
+ ? ----------------------------------------------------------
+ ?  + (Adição) => O Operador de adição, quando utilizado em 
+ ?  variáveis do tipo texto, realizará a "Concatenação"
+ ? ----------------------------------------------------------
+ ?  - (Subtração) 
+ ? * (Multiplicação) 
+ ? / (Divisão)
+ * -----------------------------------------------------------
+    
+ */
