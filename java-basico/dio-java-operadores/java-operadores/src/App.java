975e992c32749264c2a6e16ee1c04e39a582291f
Luciano.Reis <lrr.arms233@gmail.com>
2024-08-19 20:52:32 -0300

    app.class test

--- java-basico/dio-java-operadores/java-operadores/src/App.java
+++ java-basico/dio-java-operadores/java-operadores/src/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nomeCompleto = "Linguagem" + "java";
+        
+        System.out.println(nomeCompleto);
     }
 }
 
