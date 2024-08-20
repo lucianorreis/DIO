6874e4b293ebbe3bb0006f8f34ecd8a7017e2bbd
Luciano.Reis <lrr.arms233@gmail.com>
2024-08-19 21:21:36 -0300

    New File Operadores

---
+++ java-basico/dio-java-operadores/java-operadores/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 5;
+        b = 6;
+        String resultado = a==b ? "Verdadeiro" : "Falso";
+
+        System.out.println(resultado);
+    }
+}
