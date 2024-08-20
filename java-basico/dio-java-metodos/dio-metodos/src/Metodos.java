bdc9d888a325b609359a9c865430b5c3f2c530ea
Luciano.Reis <lrr.arms233@gmail.com>
2024-08-20 20:53:32 -0300

    Metodos

--- java-basico/dio-java-metodos/dio-metodos/src/Metodos.java
+++ java-basico/dio-java-metodos/dio-metodos/src/Metodos.java
@@ -1,5 +1,27 @@
 public class Metodos {
     public static void main(String[] args) {
-        
+
     } 
 }
+
+/*
+ * public class MyClass {
+ ?      public double somar(int num1, int num2) {
+ *     //LOGICA - FINALIDADE DO MÉTODO
+ ?      return . . . ;     
+ ?     }
+ * }
+ * 
+*       public void imprimir(String texto) {
+*      //LOGICA - FINALIDADE DO MÉTODO
+*      //AQUI NÃO PRECISA DE RETURN
+*      }
+* -----------------------------------------------------------------
+* // throws Exception : indica que o método ao ser utilizado
+* // poderá gerar uma exceção
+* public double dividir(int dividendo, int divisor) throws Exception{}
+ */
+/*
+* //Esse método é privado, não pode ser visto por outras classes
+?   private void metodoPrivado(){}
+ */
