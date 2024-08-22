ac79deb4835d006b96c6e9ae5b51417df461a4a4
Luciano.Reis <lrr.arms233@gmail.com>
2024-08-21 21:49:54 -0300

    updated Comentarios

--- java-basico/sistema-smart-tv/src/Comentarios.java
+++ java-basico/sistema-smart-tv/src/Comentarios.java
@@ -1,6 +1,21 @@
 public class Comentarios {
     public static void main(String[] args) {
         //Olá, eu sou um comentário em uma única linha
-        
+
+        /*
+         * Olá, eu sou um Comentário
+         * De
+         * Várias Linhas
+         */
+    }
+    /**
+     *   Estas duas estrelinhas acima
+     *   são para identificar que você
+     *   pretende elaborar um comentário 
+     *   no nível de
+     * * Documentação.
+     */
+    public void metodo(){
+
     }
 }
