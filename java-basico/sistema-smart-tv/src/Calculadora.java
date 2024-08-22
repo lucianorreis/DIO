83ff9ef47ea09290c619340d2dd4c608ff58b65c
Luciano.Reis <lrr.arms233@gmail.com>
2024-08-21 21:25:25 -0300

    JavaCalc

---
+++ java-basico/sistema-smart-tv/src/Calculadora.java
@@ -0,0 +1,11 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações entre números inteiros
+ * 
+ * @author Luciano R. Reis
+ * @version 1.0
+ * @since 21/08/2024
+ */
+public class Calculadora {
+    /** */
+}
