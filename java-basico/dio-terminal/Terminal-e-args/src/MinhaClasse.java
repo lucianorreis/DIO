c2c1abbbae2fdc320060c3fa36cc5bc8dfa2b8c5
Luciano.Reis <lrr.arms233@gmail.com>
2024-08-22 21:20:50 -0300

    New progs MinhaClasse

--- java-basico/dio-terminal/Terminal-e-args/src/MinhaClasse.java
+++ java-basico/dio-terminal/Terminal-e-args/src/MinhaClasse.java
@@ -1,5 +1,5 @@
 public class MinhaClasse {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Oi, fui executado pelo terminal");
     }
 }
