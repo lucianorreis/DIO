f262dbc74b8c505617c6db675875cfc556418a5f
Luciano.Reis <lrr.arms233@gmail.com>
2024-08-30 23:41:36 -0300

    update file ResultadoEscolar

--- java-basico/dio-java-estruturasderepetição/estruturasRepetição/src/ResultadoEscolar.java
+++ java-basico/dio-java-estruturasderepetição/estruturasRepetição/src/ResultadoEscolar.java
@@ -2,9 +2,13 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 6;
 
-        if(nota >= 7)
+        if(nota >= 7){
             System.out.println("Aprovado");
-        else
+        } else if (nota >= 5 && nota < 7){
+            System.out.println("Recuperação");
+        }else{ 
             System.out.println("Reprovado");
+        }
+           
     }
 }
