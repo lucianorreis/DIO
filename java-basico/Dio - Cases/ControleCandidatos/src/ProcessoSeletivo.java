4a94a020b485a094cced4259a28d5d568602c9fb
Luciano.Reis <lrr.arms233@gmail.com>
2024-09-04 22:01:35 -0300

    novos arquivos + atualização de nomes

---
+++ java-basico/Dio - Cases/ControleCandidatos/src/ProcessoSeletivo.java
@@ -0,0 +1,9 @@
+package candidatura;
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        System.out.println("Processo Seletivo");
+    }
+    static void analisarCandidato(double salarioPretendido){
+        
+    }
+}
