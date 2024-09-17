e4871ced292c98f45667a08da1f2dcb5d2eb8efc
Luciano.Reis <lrr.arms233@gmail.com>
2024-09-16 21:55:25 -0300

    POO - Estudo

--- trilha-java-basico/exemplo-pacotes/src/escola/Aluno.java
+++ trilha-java-basico/exemplo-pacotes/src/escola/Aluno.java
@@ -1,4 +1,18 @@
 public class Aluno {
-    String nome;
-    int idade;
+    private String nome;
+    private int idade;
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String newNome) {
+        nome = newNome;
+    }
+    
+    public int getIdade(){
+        return idade;
+    }
+    public void setIdade(int newIdade) {
+        this.idade = newIdade;
+ }
 }
