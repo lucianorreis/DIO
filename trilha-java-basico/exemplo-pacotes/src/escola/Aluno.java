a084413602cf3d459f9baf88c18a935a16c4759e
Luciano.Reis <lrr.arms233@gmail.com>
2024-09-17 22:30:28 -0300

    Constructors - Java POO

--- trilha-java-basico/exemplo-pacotes/src/escola/Aluno.java
+++ trilha-java-basico/exemplo-pacotes/src/escola/Aluno.java
@@ -2,17 +2,15 @@ public class Aluno {
     private String nome;
     private int idade;
 
+    public Aluno(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
     public String getNome(){
         return nome;
     }
-    public void setNome(String newNome) {
-        nome = newNome;
-    }
-    
     public int getIdade(){
         return idade;
     }
-    public void setIdade(int newIdade) {
-        this.idade = newIdade;
- }
+
 }
--- trilha-java-basico/exemplo-pacotes/src/escola/Escola.java
+++ trilha-java-basico/exemplo-pacotes/src/escola/Escola.java
@@ -1,9 +1,6 @@
 public class Escola {
     public static void main(String[] args) {
-        Aluno felipe = new Aluno();
-
-        felipe.setNome("Felipe"); 
-        felipe.setIdade(8);
+        Aluno felipe = new Aluno("Felipe", 8);
 
         System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos.");
     }
