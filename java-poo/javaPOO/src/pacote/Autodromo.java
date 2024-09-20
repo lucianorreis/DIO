fc312602566e122883e472570166d8e6c204d7f3
Luciano.Reis <lrr.arms233@gmail.com>
2024-09-19 23:58:10 -0300

    Novos arquivos e correções

---
+++ java-poo/javaPOO/src/pacote/Autodromo.java
@@ -0,0 +1,9 @@
+package pacote;
+
+public class Autodromo {
+    public static void main(String[] args) {
+        Carro jeep = new Carro();
+
+        jeep.ligar();
+    }
+}
---
+++ java-poo/javaPOO/src/pacote/Carro.java
@@ -0,0 +1,11 @@
+package pacote;
+
+public class Carro {
+    public void ligar() {
+        System.out.println("Ligado...");
+    }
+
+    public void confereCombustivel() {
+        System.out.println("Conferindo Combustível...");
+    }
+}
