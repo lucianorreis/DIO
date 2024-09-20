3bf05099c1be706d05eff7d14830b57b54d2e453
Luciano.Reis <lrr.arms233@gmail.com>
2024-09-19 23:48:56 -0300

    Nova Pasta Java-Poo

--- trilha-java-basico/exemplo-pacotes/src/escola/Escola.java
+++ trilha-java-basico/exemplo-pacotes/src/escola/Escola.java
@@ -1,7 +1,7 @@
 public class Escola {
     public static void main(String[] args) {
         Aluno felipe = new Aluno("Felipe", 8);
-
+        
         System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos.");
     }
 }
