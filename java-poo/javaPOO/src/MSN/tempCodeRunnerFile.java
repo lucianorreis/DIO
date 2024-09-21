19cb7b29a559eac88fbb0dd18295c1c8a92ef0cf
Luciano.Reis <lrr.arms233@gmail.com>
2024-09-20 21:23:13 -0300

    deleted Class

--- java-poo/javaPOO/src/MSN/tempCodeRunnerFile.java
+++
@@ -1,9 +0,0 @@
-package MSN;
-
-public class MSN{
-  public void validarInternet(){
-    System.out.println("Validando Conexão. . .");
-    for (int i = 5; i > 1; i--){
-        System.out.println(i + "...");
-    }
-  }
